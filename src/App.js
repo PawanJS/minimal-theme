22058d8870306633d2e50fe55e9a0253a498ff2e
PawanJS <contactpawanjs@gmail.com>
2021-11-24 20:25:26 +0530

    added package json file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import CartToolTip from './components/CartToolTip';
 
-const rootEl = document.getElementById('react-cart-tool-tip');
+const cartIconContainer = document.getElementById('react-cart-tool-tip');
 
-rootEl && ReactDOM.render(<CartToolTip />, rootEl);
+cartIconContainer && ReactDOM.render(<CartToolTip />, cartIconContainer);
